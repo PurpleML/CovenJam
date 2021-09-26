b53644290f9e7c2a3cb962afb1337ea25b6522dc
PurpleML <purplewitchml@gmail.com>
2021-09-26 16:23:43 -0500

    title updated

--- BetweenGame/Assets/Scripts/MainMenu.cs
+++ BetweenGame/Assets/Scripts/MainMenu.cs
@@ -3,13 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+using UnityEngine.UI;
+
 public class MainMenu : MonoBehaviour
 {
 
+    public GameObject titlePanel;
+    public GameObject controlsPanel;
 
     public void PlayClicked()
     {
         Debug.Log("Play");
+        titlePanel.SetActive(false);
+        controlsPanel.SetActive(true);
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void StartClicked()
+    {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
