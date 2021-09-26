37490926015be542b76169c2b2952f7ae25455ff
PurpleML <purplewitchml@gmail.com>
2021-09-26 01:02:40 -0500

    Working on level

--- BetweenGame/Assets/Scripts/Lock.cs
+++ BetweenGame/Assets/Scripts/Lock.cs
@@ -12,8 +12,8 @@ using UnityEngine;
 public class Lock : MonoBehaviour
 {
     [SerializeField] private GameObject key;
-    [SerializeField] private GameObject locked;
-    [SerializeField] private GameObject unlocked;
+    [SerializeField] private List<GameObject> locked;
+    [SerializeField] private List<GameObject> unlocked;
 
     // Start is called before the first frame update
     void Start()
@@ -31,8 +31,12 @@ public class Lock : MonoBehaviour
     {
         if (collision.gameObject.Equals(key))
         {
-            unlocked.SetActive(true);
-            locked.SetActive(false);
+            foreach(GameObject unlock in unlocked) {
+                unlock.SetActive(true);
+            }
+            foreach(GameObject lockee in locked){
+                lockee.SetActive(false);
+            }
         }
     }
 }
